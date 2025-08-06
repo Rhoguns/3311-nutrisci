ac791462658175e84cfb670f8f0a4962b64af9ab
Rhoguns <92191156+Rhoguns@users.noreply.github.com>
2025-08-05 20:26:14 -0400

    Update ProfileMain.java

--- src/main/java/com/nutrisci/app/ProfileMain.java
+++ src/main/java/com/nutrisci/app/ProfileMain.java
@@ -3,6 +3,6 @@ package com.nutrisci.app;
 
 public class ProfileMain {
     public static void main(String[] stringArray) {
-        throw new Error("Unresolved compilation problems: \n\tCannot instantiate the type ProfileDAO\n\tThe constructor Profile(String, String, LocalDate, double, double, String) is undefined\n");
+        throw new Error("undefined");
     }
 }
